bf916df72ce759e7a910a63fa8f3fce2a6770aa6
Sooyoung Jung <91307997+syj7421@users.noreply.github.com>
2024-09-15 00:40:37 +1000

    temporary save

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
+const textPoolElement = document.getElementById('text-pool');
+const textPoolHTML = textPoolElement.innerHTML;
+
+let highlightedHTML = '';
+let lastIdx = 0;
+let cnt = 0;
+let highlighted = [];
+
+
 document.getElementById('submit-btn').addEventListener('click', function(){
   const query =  document.getElementById('search-box').value;
   const result = searchText(query);
   console.log(result);
 })
 
+document.getElementById('next-match').addEventListener('click', function(){
+
+})
+
+document.getElementById('prev-match').addEventListener('click', function(){
+
+})
 
 function searchText(target) {
 
   if (target.length === 0) {
     return;
   }
-
+  resetPreviousSearch();
   clearSearchHighlights();
 
-  const textPoolElement = document.getElementById('text-pool');
-  const textPoolHTML = textPoolElement.innerHTML;
-  let highlightedHTML = '';
-  let lastIdx = 0;
-  let cnt = 0;
 
   const regex = new RegExp(target, 'g');
   let match;
 
   while ((match = regex.exec(textPoolHTML)) !== null) {
     highlightedHTML += textPoolHTML.substring(lastIdx, match.index);
-
+    highlighted.push(match.index);
     highlightedHTML += `<span class="highlight span-inserted-from-searching">${match[0]}</span>`;
-
     lastIdx = match.index + match[0].length;
-
     cnt++;
   }
 
   highlightedHTML += textPoolHTML.substring(lastIdx);
   textPoolElement.innerHTML = highlightedHTML;
+  console.log(highlighted);
+  document.getElementById('total-match').innerText = cnt;
 
   return cnt;
+}
+
+function resetPreviousSearch(){
+  highlightedHTML = '';
+  lastIdx = 0;
+  cnt = 0;
+  highlighted = [];
 }
 
 
